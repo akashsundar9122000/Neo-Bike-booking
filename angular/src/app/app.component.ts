b731aa5d1360d5047adf06d4c0fe20a98cdc1538
preethiitrmk <coder@fffbfafdfaddeddbadbeeefbebeedaedeeeacdcbd-0.fffbfafdfaddeddbadbeeefbebeedaedeeeacdcbd.che.svc.cluster.local>
2021-03-30 11:47:42 +0000

    smallchanges

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+//import {User} from '../user-profile/app.module';
+import { Company } from '../company.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular';
+ /* user:User;
+  constructor(){
+    this.user=new User();
+    this.user.name:"preethi";
+    this.user.email:"preethi@codelite.com";
+    this.user.password:"***";
+    this.user.age:"20";
+    this.user.phone:"1234567890";
+  }*/
+ companylist: Company[] = [
+   new Company("abc","xyz",123),
+   new Company("def","uvw",456),
+   new Company("ghi","rst",789),
+ ];
+  
 }
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { UserDashboardComponent } from './user-side/user-dashboard/user-dashboard.component';
+import { UserNavbarComponent } from './user-side/user-navbar/user-navbar.component';
+import { UserSideComponent } from './user-side/user-side.component';
+import { UserProfileComponent } from './user-side/user-profile/user-profile.component';
+import { UserProfileEditComponent } from './user-side/user-profile-edit/user-profile-edit.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserDashboardComponent,
+    UserNavbarComponent,
+    UserSideComponent,
+    UserProfileComponent,
+    UserProfileEditComponent,
+    
+    
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
