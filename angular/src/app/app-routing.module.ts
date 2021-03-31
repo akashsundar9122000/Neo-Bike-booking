52473dd8e21f80f4c46b4a7b82d66ead89aa7624
akash <ucs18106@rmd.ac.in>
2021-03-31 17:40:01 +0000

    changes

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { from } from 'rxjs';
 import { AdminDashboardComponent } from './admin-side/admin-dashboard/admin-dashboard.component';
-import {SignupComponent} from './auth/signup/signup.component'
+import {UserSignupComponent} from './auth/userSignup/userSignup.component'
 import {LoginComponent} from './auth/login/login.component'
 import {AdminProfileComponent} from './admin-side/admin-profile/admin-profile.component'
-
+import {AdminSignupComponent} from './auth/adminSignup/adminSignup.component'
+import {AdminNavbarComponent} from './admin-side/admin-navbar/admin-navbar.component'
 const routes: Routes = [
   { path: 'dashboard', component: AdminDashboardComponent },
-  { path: 'signup', component: SignupComponent },
+  { path: 'userSignup', component: UserSignupComponent },
   { path: 'login', component: LoginComponent },
-  {path : 'profile', component: AdminProfileComponent}
+  {path : 'profile', component: AdminProfileComponent},
+  { path: 'adminSignup', component: AdminSignupComponent },
+  {path: 'adminnavbar', component : AdminNavbarComponent}
 ];
 
 @NgModule({
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SignupComponent } from './auth/signup/signup.component';
+import { UserSignupComponent } from './auth/userSignup/userSignup.component';
 import { AdminSideComponent } from './admin-side/admin-side.component';
 import { AdminNavbarComponent } from './admin-side/admin-navbar/admin-navbar.component';
 import { AdminDashboardComponent } from './admin-side/admin-dashboard/admin-dashboard.component';
 import { AdminProfileComponent } from './admin-side/admin-profile/admin-profile.component';
 import { LoginComponent } from './auth/login/login.component';
 import { ModlesComponent } from './modles/modles.component';
+import { AdminSignupComponent } from './auth/adminSignup/adminSignup.component';
+
 
 @NgModule({
 
@@ -17,13 +19,15 @@ import { ModlesComponent } from './modles/modles.component';
 
   declarations: [
     AppComponent,
-    SignupComponent,
+    UserSignupComponent,
     AdminSideComponent,
     AdminNavbarComponent,
     AdminDashboardComponent,
     AdminProfileComponent,
     LoginComponent,
-    ModlesComponent
+    ModlesComponent,
+    AdminSignupComponent,
+    
   ],
   imports: [
     BrowserModule,
